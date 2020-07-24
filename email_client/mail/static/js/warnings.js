5914a818c81b089fce786fe27874a01df1f0a835
Justin <jpetelka@uw.edu>
2020-07-23 23:53:22 -0700

    Throw banner warning as <template>

--- email_client/mail/static/js/warnings.js
+++ email_client/mail/static/js/warnings.js
@@ -1,8 +1,8 @@
 function load_warning(group_num){
-    const warning_text = "<div class='row banner-w text-center'><div style='padding: 15px;'><p class=''>Warning text here</p></div></div><style>.banner-w {padding: 10px;height: 100px;border-radius: 4px;background-color: #A64452;}</style>";
-    // const warning_text = "{% include warning_fname %}"
+    var template = document.getElementsByTagName("template")[0];
+    var clon = template.content.cloneNode(true);
     if (group_num==4){
-        $(warning_text).insertBefore('.sender-info');
+        $('.sender-info').before(clon);
     }
 }
 
