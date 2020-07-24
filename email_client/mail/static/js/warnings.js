725e75255bec43f427eca61c376b79382046fc58
Justin <jpetelka@uw.edu>
2020-07-24 01:20:11 -0700

    Throw unstyled tooltip over suspect link

--- email_client/mail/static/js/warnings.js
+++ email_client/mail/static/js/warnings.js
@@ -1,16 +1,39 @@
 function load_warning(group_num){
     var template = document.getElementsByTagName("template")[0];
     var clon = template.content.cloneNode(true);
-    if (group_num==4){
+    if (group_num==1){
         $('.sender-info').before(clon);
+    }
+    else if (group_num==4){
+        var names = ["chase.co.br","https://www.yahoo.co.br/"];
+        $("#email_container a").each(function(){
+            var raw_link = $(this).attr("href");
+            // console.log(raw_link);
+            var temp = '<div class="tooltip" role="tooltip"><div class="tooltip-arrow"></div><div class="tooltip-inner warning"></div></div>';
+            var text = "Warning text goes here";
+            if (names.indexOf(raw_link) > 0){
+                $(this).attr('data-toggle', 'tooltip');
+                var trigger = 'hover';
+                //options for tooltip
+                options = {
+                    placement: 'bottom',
+                    html: true,
+                    title: text,
+                    trigger: trigger,
+                    template: temp,
+                }
+                $("[data-toggle='tooltip']").tooltip(options);
+                
+            }
+        });
     }
 }
 
 function read_email(group_num, email_id){
-    var warning_ids = [12, 3, 18];
-    group_num_int = parseInt(group_num);
-    email_id_int = parseInt(email_id);
-    if (warning_ids.includes((email_id_int))){
+    var email_ids = [12, 3, 18];
+    var group_num_int = parseInt(group_num);
+    var email_id_int = parseInt(email_id);
+    if (email_ids.includes((email_id_int))){
         load_warning(group_num_int);
     }
 }
--- email_client/mail/views.py
+++ email_client/mail/views.py
@@ -100,7 +100,6 @@ def email(request, email_id):
             #     user.save()
             # user.unread_count = F('unread_count') - 1
             # user.save()
-        
         warning_fname = 'mail/warnings/' + str(4) + '.html'
         # Find the order_number of the email being retreived
         # order_num = emails.index()
