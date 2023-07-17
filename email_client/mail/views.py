3a4d938d71b338e643f53120cf0e34a1abe49a8d
Justin <jpetelka@uw.edu>
2023-07-17 16:54:18 -0700

    added HTTP_ to headers. maybe this will work

--- email_client/mail/views.py
+++ email_client/mail/views.py
@@ -431,7 +431,7 @@ def assign_credentials(request):
         # Return a list of the remaining available usernames
         # cast the group_num variable from qualtrics to an integer
         # print(request.GET.keys())
-        qual_group_num = int(request.META['GROUP_NUM'])
+        qual_group_num = int(request.META['HTTP_GROUP_NUM'])
         users = User.objects.filter(assigned=False, group_num=qual_group_num).filter(is_superuser=False)
         # users is a QuerySet. Calling len() on users caches the whole database selection at once
         # This avoids multiple db calls
@@ -450,8 +450,8 @@ def assign_credentials(request):
                 user_index = rd.randint(0,len_users-1)
                 user = users[user_index]
             # Save the response_id from Qualtrics
-            if (request.META("RESPONSE_ID")):
-                user.response_id = request.META("RESPONSE_ID")
+            if (request.META("HTTP_RESPONSE_ID")):
+                user.response_id = request.META("HTTP_RESPONSE_ID")
             username = user.username
             password = assign_password()
             # Mark the username as 'assigned'
